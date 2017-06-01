9e300725d73cdd1758a169c0b557d59defd09b04
Nick Carboni <ncarboni@redhat.com>
2017-06-01 17:49:45 -0400

    First pass

---
+++ images/ansible-tower/docker-assets/initialize-tower.sh
@@ -0,0 +1,27 @@
+# Print the inventory file
+cat << EOF > /inventory
+[tower]
+localhost ansible_connection=local
+
+[database]
+
+[all:vars]
+admin_password='${ADMIN_PASSWORD}'
+
+pg_host='${DATABASE_SERVICE_NAME}'
+pg_port='5432'
+
+pg_database='${DATABASE_NAME}'
+pg_username='${POSTGRESQL_USER}'
+pg_password='${POSTGRESQL_PASSWORD}'
+
+rabbitmq_port=5672
+rabbitmq_vhost=tower
+rabbitmq_username='${RABBITMQ_USER_NAME}'
+rabbitmq_password='${RABBITMQ_PASSWORD}'
+rabbitmq_cookie=cookiemonster
+rabbitmq_use_long_name=false
+rabbitmq_enable_manager=false
+EOF
+
+ansible-tower-setup -e minimum_var_space=0 --inventory=/inventory
